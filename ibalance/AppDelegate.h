46b9ac7cdeaeef628293dfaddcaab65102b9ee1d
Sam <sam@samnovotny.com>
2012-11-04 01:06:07 +0000

    Initial Commit

---
+++ ibalance/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ibalance
+//
+//  Created by Sam on 04/11/2012.
+//  Copyright (c) 2012 Sam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ibalance/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ibalance
+//
+//  Created by Sam on 04/11/2012.
+//  Copyright (c) 2012 Sam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
